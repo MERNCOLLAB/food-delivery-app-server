23041edc843dc93f73b821edd2a06d480a980323
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-07-03 13:48:22 +0800

    🔧 fix: order and payment relationship

--- models/order.go
+++ models/order.go
@@ -17,6 +17,7 @@ type Order struct {
 	PlacedAt        time.Time  `gorm:"autoCreateTime" json:"placedAt"`
 	UpdatedAt       time.Time  `gorm:"autoUpdateTime" json:"updatedAt"`
 
+	Payment    *Payment    `gorm:"foreignKey:OrderID" json:"payment,omitempty"`
 	Restaurant *Restaurant `gorm:"foreignKey:RestaurantID;references:ID;constraint:OnDelete:CASCADE" json:"restaurant,omitempty"`
 	Customer   *User       `gorm:"-" json:"customer,omitempty"`
 	Driver     *User       `gorm:"-" json:"driver,omitempty"`
